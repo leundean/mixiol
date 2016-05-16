501024f8687dfc108d42078e4ea96197fe3ee588
Mikael <leundean@gmail.com>
2016-05-16 13:32:56 +0200

    Added some Java code

---
+++ Java.java
@@ -0,0 +1,8 @@
+public class Java {
+	public static void main(String[] args){
+		for (int i=0; i < args.length; i++){
+			System.out.println("Arg " +i+ ": " + args[i]);
+		}
+		System.out.println("END");
+	}
+}
